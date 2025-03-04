0fc3f9dfe31dc3d8de7d22944e98e08545287d40
detautama <detautama11@gmail.com>
2025-03-05 03:24:35 +0800

    Refactor layout components to use a dedicated Header component for improved structure and maintainability

--- app/(home-layout)/layout.tsx
+++ app/(home-layout)/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
-import Image from "next/image";
 import "../globals.css";
 import Link from "next/link";
 import { getTopTags } from "../lib/articles";
-import { ToggleDarkMode } from "../ToggleDarkMode";
+import { Header } from "../server-components/Header";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://detautama.me/"),
@@ -21,48 +20,7 @@ export default function RootLayout({
   });
   return (
     <div className="mx-auto max-w-4xl px-4">
-      <div
-        id="header"
-        className="flex flex-col items-center justify-between gap-4 py-5 md:flex-row"
-      >
-        <div
-          id="header-left"
-          className="flex flex-col items-center gap-4 md:flex-row"
-        >
-          <Link href="/">
-            <Image
-              className="shimmer"
-              src="/deta.png"
-              alt="profile image"
-              width={70}
-              height={70}
-            />
-          </Link>
-          <div id="tagline" className="text-center md:text-left">
-            <Link href="/" className="text-xl font-bold hover:underline">
-              I Putu Deta Utama Putra
-            </Link>
-            <small className="mb-3 block">ᬇ​ᬧᬸᬢᬸ​ᬤᬾᬢ​ᬉᬢᬫ​ᬧᬸᬢ᭄ᬭ</small>
-            <p>A blog on JavaScript, web, and more</p>
-          </div>
-        </div>
-        <div id="header-right" className="flex gap-4">
-          <Link href={"/search"}>
-            <Image
-              src="/images/search.svg"
-              alt="search icon"
-              width={20}
-              height={20}
-            />
-          </Link>
-          <ToggleDarkMode />
-        </div>
-      </div>
-      <div className="default_bg mb-6 flex w-full justify-center gap-6 rounded-md p-0.5 font-bold">
-        <Link href="/">Articles</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/about">About</Link>
-      </div>
+      <Header />
       <div className="flex flex-col gap-4 md:flex-row">
         <div className="w-full md:w-3/4">{children}</div>
         <div id="sidebar" className="flex w-full flex-col gap-4 md:w-1/4">
