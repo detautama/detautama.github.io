37d4a5bb7bb153e3ed81f573c7cb9cad241a86c6
detautama <detautama11@gmail.com>
2025-05-14 15:21:49 +0800

    Add Google Tag Manager and Google Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <GoogleTagManager gtmId="G-74BLMRKJD6" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
