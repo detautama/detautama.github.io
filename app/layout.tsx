8315d162f97402b1ab82db600062d43afc016d25
detautama <detautama11@gmail.com>
2025-05-14 15:52:30 +0800

    Remove Google Tag Manager and retain Google Analytics integration in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <GoogleTagManager gtmId="G-74BLMRKJD6" />
+      <GoogleAnalytics gaId="G-74BLMRKJD6" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
